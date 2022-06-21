c3e9e9c9fbe8a9a9a9b40cc6593f8f136e63439e
Hiệp Đinh Quảng <quanghiep2109@gmail.com>
2022-06-21 18:18:30 +0700

    21/6 6:18

--- PBL3-Remake/Form1.Designer.cs
+++ PBL3-Remake/Form1.Designer.cs
@@ -29,36 +29,36 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.timeSheetUC = new GUI.frmGUIUserControl.TimeSheetUC();
+            this.timeSheetUC1 = new GUI.frmGUIUserControl.TimeSheetUC();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(445, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(593, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // timeSheetUC
+            // timeSheetUC1
             // 
-            this.timeSheetUC.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.timeSheetUC.Location = new System.Drawing.Point(148, 35);
-            this.timeSheetUC.Name = "timeSheetUC";
-            this.timeSheetUC.Size = new System.Drawing.Size(618, 391);
-            this.timeSheetUC.TabIndex = 2;
+            this.timeSheetUC1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(59)))));
+            this.timeSheetUC1.Location = new System.Drawing.Point(298, 104);
+            this.timeSheetUC1.Name = "timeSheetUC1";
+            this.timeSheetUC1.Size = new System.Drawing.Size(600, 430);
+            this.timeSheetUC1.TabIndex = 2;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(931, 493);
-            this.Controls.Add(this.timeSheetUC);
+            this.ClientSize = new System.Drawing.Size(1241, 607);
+            this.Controls.Add(this.timeSheetUC1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -68,6 +68,6 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private GUI.frmGUIUserControl.TimeSheetUC timeSheetUC;
+        private frmGUIUserControl.TimeSheetUC timeSheetUC1;
     }
 }
